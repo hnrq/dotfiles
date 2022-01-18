f84b3e744aafa34199720cc5702a9aaa66f8432a
Henrique Ramos <henriquealberone@outlook.com>
2022-01-18 13:37:50 -0300

    Add install-once-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,13 +5,18 @@ sudo pacman -S reflector && reflector --latest 200 --protocol \
 	http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages required for dotfiles
-sudo pacman -S --needed --no-confirm python-pywal xorg go fish neovim \
+sudo pacman -S --needed --no-confirm python-pywal xorg go neovim \
   bspwm sxhkd polybar picom htop rxvt-unicode feh ranger rofi \
-  networkmanager network-manager-applet git fish telegram-desktop 
+  networkmanager network-manager-applet git fish telegram-desktop \
+  devmon zsh
+
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting
 
 # Install yay
 git clone --quiet https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 rm -rf yay
 
 # Install AUR packages
-yay -S --noconfirm fisher visual-studio-code-bin google-chrome 
+yay -S --noconfirm visual-studio-code-bin google-chrome cozette-ttf
