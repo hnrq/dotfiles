541c0f7f3292bb17456eb24dba442eee94466a47
Henrique Ramos <henriquealberone@outlook.com>
2022-01-30 01:48:24 -0300

    Remove background GLX from picom

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-# Fetches best mirror
-sudo pacman -S reflector && reflector --latest 200 --protocol \
-	http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-
-# Packages required for dotfiles
-sudo pacman -S --needed --no-confirm python-pywal xorg go neovim \
-  bspwm sxhkd polybar picom htop rxvt-unicode feh ranger rofi \
-  networkmanager network-manager-applet git fish telegram-desktop \
-  devmon zsh
-
-# Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting
-
-# Install yay
 git clone --quiet https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 rm -rf yay
 
+# Fetches best mirror
+sudo pacman -S --needed --noconfirm reflector && reflector --latest 200 --protocol \
+	http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Packages required for dotfiles
+sudo pacman -S --needed --noconfirm python-pywal xorg go neovim \
+  bspwm sxhkd polybar picom htop rxvt-unicode feh ranger rofi \
+  networkmanager network-manager-applet git fish telegram-desktop \
+  devmon zsh zsh-autosuggestions zsh-syntax-highlighting
+
+
 # Install AUR packages
-yay -S --noconfirm visual-studio-code-bin google-chrome cozette-ttf
+yay -S --noconfirm cozette-ttf
