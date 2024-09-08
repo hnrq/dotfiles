b87b463ef2ca774ca942d92ba71328ef6120891d
Henrique Ramos <henriquealberone@outlook.com>
2024-09-07 22:32:05 -0300

    Update .config/screenlock.sh

--- dot_config/executable_screenlock.sh
+++ dot_config/executable_screenlock.sh
@@ -1,7 +1,8 @@
 # xsecurelock options export
-export XSECURELOCK_SAVER=saver_xscreensaver
+# export XSECURELOCK_SAVER=saver_xscreensaver
 export XSECURELOCK_SHOW_DATETIME=1
 export XSECURELOCK_SINGLE_AUTH_WINDOW=1
 export XSECURELOCK_AUTH_TIMEOUT=30
 export XSECURELOCK_FONT="Unifont"
+export XSECURELOCK_PASSWORD_PROMPT="disco"
 xsecurelock
