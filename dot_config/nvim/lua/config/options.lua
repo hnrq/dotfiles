af5365f9a0b380f5dc486e8ef99df62553f96bd1
Henrique Ramos <henriquealberone@outlook.com>
2025-02-12 13:30:20 -0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/autocomplete.lua Add .config/nvim/lua/plugins/rust.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -64,3 +64,12 @@ precedes = "⟨",
 nbsp = "␣",
 }
 
+-- Autocomplete
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300)
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
