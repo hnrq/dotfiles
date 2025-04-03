6a610808fa960455cf6146db7bc1ab21a6cbca4c
Henrique Ramos <henriquealberone@outlook.com>
2025-04-02 22:34:51 -0300

    Update .config/polybar/config.ini Update .config/polybar/launch.sh

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -8,5 +8,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  done
+else
+  polybar -c ~/.config/polybar/config.ini main &
+fi
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
