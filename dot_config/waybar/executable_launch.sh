288effac4ca88364d804be58e02a2e75c1936567
Henrique Ramos <henriquealberone@outlook.com>
2025-06-30 12:57:24 -0300

    Update .config/waybar/launch.sh

--- dot_config/waybar/executable_launch.sh
+++ dot_config/waybar/executable_launch.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-#                    __
-#  _    _____ ___ __/ /  ___ _____
-# | |/|/ / _ `/ // / _ \/ _ `/ __/
-# |__,__/\_,_/\_, /_.__/\_,_/_/
-#            /___/
-#
-
-# -----------------------------------------------------
-# Prevent duplicate launches: only the first parallel
-# invocation proceeds; all others exit immediately.
-# -----------------------------------------------------
-exec 200>/tmp/waybar-launch.lock
-flock -n 200 || exit 0
-
-# -----------------------------------------------------
-# Quit all running waybar instances
-# -----------------------------------------------------
-killall waybar || true
-pkill waybar || true
-sleep 0.5
-
-# Execute waybar
-waybar
-
-# Explicitly release the lock (optional) -> flock releases on exit
-flock -u 200
-exec 200>&-
+waybar&
+trap "killall waybar" EXIT
+while inotifywait -r -e create,modify ~/.config/waybar/*; do killall -SIGUSR2 waybar; done
