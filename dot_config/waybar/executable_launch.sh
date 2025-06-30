11feaa2dcdbd06aa5664930eefb9ae9c13e8d6a1
Henrique Ramos <henriquealberone@outlook.com>
2025-06-30 12:52:47 -0300

    Update .config/waybar/launch.sh

--- dot_config/waybar/executable_launch.sh
+++ dot_config/waybar/executable_launch.sh
@@ -20,48 +20,8 @@ killall waybar || true
 pkill waybar || true
 sleep 0.5
 
-# -----------------------------------------------------
-# Default theme: /THEMEFOLDER;/VARIATION
-# -----------------------------------------------------
-themestyle="/ml4w-modern;/ml4w-modern/light"
-
-# -----------------------------------------------------
-# Get current theme information from ~/.config/ml4w/settings/waybar-theme.sh
-# -----------------------------------------------------
-if [ -f ~/.config/ml4w/settings/waybar-theme.sh ]; then
-    themestyle=$(cat ~/.config/ml4w/settings/waybar-theme.sh)
-else
-    touch ~/.config/ml4w/settings/waybar-theme.sh
-    echo "$themestyle" >~/.config/ml4w/settings/waybar-theme.sh
-fi
-
-IFS=';' read -ra arrThemes <<<"$themestyle"
-echo ":: Theme: ${arrThemes[0]}"
-
-if [ ! -f ~/.config/waybar/themes${arrThemes[1]}/style.css ]; then
-    themestyle="/ml4w;/ml4w/light"
-fi
-
-# -----------------------------------------------------
-# Loading the configuration
-# -----------------------------------------------------
-config_file="config"
-style_file="style.css"
-
-# Standard files can be overwritten with an existing config-custom or style-custom.css
-if [ -f ~/.config/waybar/themes${arrThemes[0]}/config-custom ]; then
-    config_file="config-custom"
-fi
-if [ -f ~/.config/waybar/themes${arrThemes[1]}/style-custom.css ]; then
-    style_file="style-custom.css"
-fi
-
-# Check if waybar-disabled file exists
-if [ ! -f $HOME/.config/ml4w/settings/waybar-disabled ]; then
-    waybar -c ~/.config/waybar/themes${arrThemes[0]}/$config_file -s ~/.config/waybar/themes${arrThemes[1]}/$style_file &
-else
-    echo ":: Waybar disabled"
-fi
+# Execute waybar
+waybar
 
 # Explicitly release the lock (optional) -> flock releases on exit
 flock -u 200
