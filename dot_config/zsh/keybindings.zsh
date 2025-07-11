bfac42761d491b6014cd9aa9537bca00b50ab81d
Henrique Ramos <henriquealberone@outlook.com>
2025-07-11 11:12:37 -0300

    Update .config/zsh/keybindings.zsh Update .config/zsh/plugins.txt Update .config/zsh/plugins.zsh

--- dot_config/zsh/keybindings.zsh
+++ dot_config/zsh/keybindings.zsh
@@ -1,3 +1,6 @@
 # ctrl+arrow navigation
 bindkey '^[[1;5C' forward-word
 bindkey '^[[1;5D' backward-word
+bindkey  "^[[H"   beginning-of-line
+bindkey  "^[[F"   end-of-line
+bindkey  "^[[3~"  delete-char
