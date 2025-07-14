46f3405710f5f28d6854752e16ab0f262131bf31
Henrique Ramos <henriquealberone@outlook.com>
2025-07-14 12:14:29 -0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/ui.lua

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -10,27 +10,23 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "auto",
-          component_separators = "|",
-          section_separators = "",
-        },
-      })
-    end,
+    options = {
+      theme = "auto",
+      component_separators = "|",
+      section_separators = "",
+    },
   },
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    opts = {
+      win = {
+        border = "single"
+      }
+    },
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
-      require("which-key").setup({
-        win = {
-          border = "single",
-        },
-      })
     end,
   },
   {
@@ -41,20 +37,24 @@ return {
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-            hide_gitignored = false,
-          },
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
         },
-        window = {
-          position = "left",
-          width = 30,
+        use_libuv_file_watcher = true,
+        follow_current_file = {
+          enabled = true,
         },
-      })
-    end,
+      },
+      window = {
+        position = "left",
+        width = 30,
+      },
+    }
   },
 }
